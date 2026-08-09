dac5a3db990a97915c8d8b20da767c315c968bb2
TBERT31 <berteau.thomas@hotmail.fr>
2026-08-09 17:35:30 +0200

    Just try to work with Polaris

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -19,17 +19,33 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { admin } = await authenticate.admin(request);
   
+  console.log("Action called with admin:", admin);
   return null;
 };
 
 export default function Index() {
   const fetcher = useFetcher<typeof action>();
+  const submitHandler = () => {
+    fetcher.submit(null, { method: "post" });
+  }; 
 
   return (
-    <s-page heading="Shopify app template">
-      <s-section heading="Congrats on creating a new Shopify app 🎉">
-       
-      </s-section>
+    <s-page heading="LIKE PRODUCT">
+      <s-stack gap="base large-300">
+
+        <s-section heading="[[NAME]] Welcome to LIKE PRODUCTS app!">
+          <s-paragraph>Here you can enable likes for your products.</s-paragraph>
+        </s-section>
+
+        <s-heading>Status</s-heading>
+
+        <s-stack gap="base large-200" rowGap="large-200" direction="inline">
+          <s-section heading="Total likes">
+            <s-paragraph>140</s-paragraph>
+          </s-section>
+        </s-stack>
+
+      </s-stack>
     </s-page>
   );
 }
