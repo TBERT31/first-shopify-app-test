18ab6448c5564578d1c5d16f64745ae503529b53
TBERT31 <berteau.thomas@hotmail.fr>
2026-08-10 10:03:52 +0200

    Update plans and settings pages

--- app/routes/app.plans.tsx
+++ app/routes/app.plans.tsx
@@ -1,37 +1,20 @@
-export default function AdditionalPage() {
+import { PricingCard } from "app/components";
+
+export default function PlansPage() {
   return (
-    <s-page heading="Additional page">
-      <s-section heading="Multiple pages">
-        <s-paragraph>
-          The app template comes with an additional page which demonstrates how
-          to create multiple pages within app navigation using{" "}
-          <s-link
-            href="https://shopify.dev/docs/apps/tools/app-bridge"
-            target="_blank"
-          >
-            App Bridge
-          </s-link>
-          .
-        </s-paragraph>
-        <s-paragraph>
-          To create your own page and have it show up in the app navigation, add
-          a page inside <code>app/routes</code>, and a link to it in the{" "}
-          <code>&lt;ui-nav-menu&gt;</code> component found in{" "}
-          <code>app/routes/app.jsx</code>.
-        </s-paragraph>
-      </s-section>
-      <s-section slot="aside" heading="Resources">
-        <s-unordered-list>
-          <s-list-item>
-            <s-link
-              href="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-              target="_blank"
-            >
-              App nav best practices
-            </s-link>
-          </s-list-item>
-        </s-unordered-list>
-      </s-section>
+    <s-page heading="Plans">
+        <s-stack gap="base large-300">
+          <s-heading>Our Plans</s-heading>
+          <PricingCard
+            title="Unique Plan"
+            description="The unique plan for now, other plans will be available soon."
+            price="$5"
+            features={["Many likes as possible", "Email support", "Analytics"]}
+            featuredText="Popular"
+            button={{content: "Select Plan"}}
+            frequency="month"
+          />
+        </s-stack>
     </s-page>
   );
 }
--- app/routes/app.settings.tsx
+++ app/routes/app.settings.tsx
@@ -1,6 +1,6 @@
-export default function AnythingPage() {
+export default function SettingsPage() {
   return (
-    <s-page heading="Anything page">
+    <s-page heading="Settings">
       <s-section heading="Multiple pages">
         <s-paragraph>
           This is an anything page to demonstrate routing capabilities.
